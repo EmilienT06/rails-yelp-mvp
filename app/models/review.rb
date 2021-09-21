14637eb1ad8db10a438f331d9aad25b2dfc7fbf6
Emilien Testu <emilientestu.pro@gmail.com>
2021-09-21 20:32:43 +0200

    Fix validation Restaurant & Review models

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, :rating, :restaurant, presence: true
-  validates :rating, inclusion: { in: [0..5],
-                                  message: '%{value} must be within 0 to 5 included'}
+  validates :rating, inclusion: { in: 0..5,
+                                  message: '%{value} must be within 0 to 5 included'}, numericality: true
 end
