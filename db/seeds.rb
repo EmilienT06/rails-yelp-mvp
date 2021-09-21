4faf26defdf3e160c987acbb6c54e04a39e90fc7
Emilien Testu <emilientestu.pro@gmail.com>
2021-09-21 21:16:43 +0200

    Seed my app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+porteno = Restaurant.create(name: 'Porteno', address: '4 Rue Lepic', category: 'italian')
+mama = Restaurant.create(name: 'Mama Pizza', address: '50 Rue Dorée', category: 'italian', phone_number: '+3346456')
+pekin = Restaurant.create(name: 'Pekin Food', address: '5R5 Contonese Street', category: 'chinese')
+tartare = Restaurant.create(name: 'Les Tontons', address: '4 Rue R.Losserand', category: 'french')
+moule = Restaurant.create(name: 'Moulue', address: '4 Rue Atomium', category: 'beligian')
+
+Review.create(content: 'Great', rating: 4, restaurant_id: porteno.id)
+Review.create(content: 'Average', rating: 2, restaurant_id: mama.id)
+Review.create(content: 'Bad', rating: 1, restaurant_id: pekin.id)
+Review.create(content: 'Amazing', rating: 5, restaurant_id: tartare.id)
+Review.create(content: 'Disgusting', rating: 0, restaurant_id: moule.id)
