7ed6d468d2b1d88c5ccfa85716e643d46a5559cb
Emilien Testu <emilientestu.pro@gmail.com>
2021-09-21 20:18:29 +0200

    Set validation & association for Restaurant & Review models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, :rating, :restaurant, presence: true
+  validates :rating, inclusion: { in: [0..5],
+                                  message: '%{value} must be within 0 to 5 included'}
 end
